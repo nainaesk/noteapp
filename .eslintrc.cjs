2063dd85d7b947f5b3135cdd26e59d70908075a9
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-13 23:47:30 +0530

    chore: configure tsconfig, eslint, and folder structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+const { rules } = require("eslint-plugin-react-refresh");
+
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        '@electron-toolkit/eslint-config-ts/recommended',
+        '@electron-toolkit/eslint-config-prettier'
+    ],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+    }
+}
---
+++ src/preload/index.ts
@@ -0,0 +1,18 @@
+import { contextBridge } from 'electron'
+
+
+
+// Use `contextBridge` APIs to expose Electron APIs to
+// renderer only if context isolation is enabled, otherwise
+// just add to the DOM global.
+if (process.contextIsolated) {
+  try {
+    contextBridge.exposeInMainWorld('context', {
+      // TODO
+    })
+  } catch (error) {
+    console.error(error)
+  }
+} else {
+  throw new Error('contextIsolation must be enabled in BrowserWindow')
+}
