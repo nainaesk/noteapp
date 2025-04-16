757b2c82194de3eeec0f83f279a978b8767dcfe4
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-16 19:08:31 +0530

    Feat: Draggable top bar

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,10 @@
 import './app.css'
-import { Content, RootLayout, SideBar } from './components'
-function App() {
+import { Content, DraggableTopBar, RootLayout, SideBar } from './components'
+
+const App = () => {
   return (
     <>
+      <DraggableTopBar />
       <RootLayout>
         <SideBar className="p-2">Sidebar</SideBar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
