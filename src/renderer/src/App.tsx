1449df63e503edd46053d89e0083d1e678809fe9
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-16 21:48:41 +0530

    Feat: Added editor added tailwind typography

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,6 +7,7 @@ import {
   SideBar
 } from '@/components'
 import './app.css'
+import { MarkdowEditor } from './components/MarkdowEditor'
 
 const App = () => {
   return (
@@ -17,7 +18,9 @@ const App = () => {
           <ActionButtonsRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-3 space-y-1" />
         </SideBar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <MarkdowEditor />
+        </Content>
       </RootLayout>
     </>
   )
