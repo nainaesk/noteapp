4574943bdb4711bf76ca54fcb6cf17fd6f144b7c
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-16 19:00:45 +0530

    Feat: App layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,12 @@
+import './app.css'
+import { Content, RootLayout, SideBar } from './components'
 function App() {
   return (
     <>
-      <div className="flex h-full items-center justify-center">
-        <span className="text-4xl text-blue-500">Hello from Electron</span>
-      </div>
+      <RootLayout>
+        <SideBar className="p-2">Sidebar</SideBar>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+      </RootLayout>
     </>
   )
 }
