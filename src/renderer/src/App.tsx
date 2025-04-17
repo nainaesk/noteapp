03f417b7c7fa472eacb6fbdf9f1e26552572b2b5
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-17 11:24:22 +0530

    Feat: User can view selected  note, on select note scroll up

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,19 +7,26 @@ import {
   RootLayout,
   SideBar
 } from '@/components'
+import { useRef } from 'react'
 import './app.css'
 import { MarkdowEditor } from './components/MarkdowEditor'
 
 const App = () => {
+  const contentContainerRef = useRef<HTMLDivElement>(null)
+
+  const resetScroll = () => {
+    contentContainerRef.current?.scrollTo(0, 0)
+  }
+
   return (
     <>
       <DraggableTopBar />
       <RootLayout>
         <SideBar className="p-2">
           <ActionButtonsRow className="flex justify-between mt-1" />
-          <NotePreviewList className="mt-3 space-y-1" />
+          <NotePreviewList className="mt-3 space-y-1" onSelect={resetScroll} />
         </SideBar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+        <Content ref={contentContainerRef} className="border-l bg-zinc-900/50 border-l-white/20">
           <FloatingNoteTitle className="pt-2" />
           <MarkdowEditor />
         </Content>
---
+++ src/renderer/src/hooks/useMarkDownEditor.tsx
@@ -0,0 +1,10 @@
+import { selectedNoteAtom } from '@renderer/store'
+import { useAtomValue } from 'jotai'
+
+export const useMarkDownEditor = () => {
+  const selectedNote = useAtomValue(selectedNoteAtom)
+
+  return {
+    selectedNote
+  }
+}
