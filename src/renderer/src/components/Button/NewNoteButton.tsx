b930e39937e672cc0e8f478dc9df6f397e2a2436
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-17 12:17:15 +0530

    Feat: Delete and Add Note

--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,9 +1,16 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@renderer/store'
+import { useAtom } from 'jotai'
 import { LuSignature } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const [_, createEmptyNote] = useAtom(createEmptyNoteAtom)
+
+  const handleCreation = () => {
+    createEmptyNote()
+  }
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreation} {...props}>
       <LuSignature className="w-4 h-4 text-zinc-300"></LuSignature>
     </ActionButton>
   )
